229ce21dfe25fa07c988f79272c31a018f84f13d
nerdeveloper <odirionye@gmail.com>
2017-11-27 00:00:45 +0100

    created my first open source library

---
+++ webpack.config.babel.js
@@ -0,0 +1,19 @@
+import {join} from 'path'
+
+const include = join(__dirname, 'src')
+
+export default {
+  entry: './src/index',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'game_of_thronesNames',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel', include},
+      {test: /\.json$/, 'loader': 'json', include},
+    ]
+  }
+}
